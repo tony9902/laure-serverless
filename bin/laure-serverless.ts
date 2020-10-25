f5ef2431282fa4e029dbbea48df69f185995f2f9
Yefri <yefrioscar9814@gmail.com>
2020-10-25 16:42:56 -0500

    update

--- bin/laure-serverless.ts
+++ bin/laure-serverless.ts
@@ -2,6 +2,11 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { LaureServerlessStack } from '../lib/laure-serverless-stack';
+import { LaureResourcesServerlessStack } from '../lib/laure-dashboard-resources.stack';
 
 const app = new cdk.App();
 new LaureServerlessStack(app, 'LaureServerlessStack');
+
+
+const app2 = new cdk.App();
+new LaureResourcesServerlessStack(app2, 'LaureResourcesServerlessStack');
