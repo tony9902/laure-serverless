78db08653b91eef45ac3156e4a3e18e49ad6d29a
antony <laurameneses@gmail.com>
2020-10-25 14:13:08 -0500

    add

--- lambda/products/add.ts
+++ lambda/products/add.ts
@@ -1,9 +1,14 @@
 import * as admin from "firebase-admin";
+const { DynamoDB, Lambda } = require("aws-sdk");
+import * as aws from "aws-sdk";
+import { PutObjectRequest } from "aws-sdk/clients/s3";
+const s3= new aws.S3(); 
+const BUCKET_NAME = process.env.BUCKET_NAME + "";
 
 const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID + "";
 const FIREBASE_PRIVATE_KEY = process.env.FIREBASE_PRIVATE_KEY + "";
 const FIREBASE_CLIENT_EMAIL = process.env.FIREBASE_CLIENT_EMAIL + "";
-const params = {
+const paramsfirebase = {
     projectId: FIREBASE_PROJECT_ID,
     private_key: FIREBASE_PRIVATE_KEY,
     client_email: FIREBASE_CLIENT_EMAIL,
@@ -11,7 +16,7 @@ const params = {
 exports.handler = async function (event:any) {
   try {
     admin.initializeApp({
-        credential: admin.credential.cert(params),
+        credential: admin.credential.cert(paramsfirebase),
         databaseURL: "https://laure-shop.firebaseio.com",
     });
 } catch (error) {
@@ -24,22 +29,33 @@ exports.handler = async function (event:any) {
         console.error("Firebase admin initialization error", error.stack);
     }
 }
+const params: PutObjectRequest = {
+  Key:"images/",
+  Bucket: BUCKET_NAME,
+};
 
-  let {
-     brand, category, name, price_pen, price_usd
-  }=event
-  
-  try {
-    const collection = await admin.firestore().collection("products").add(
-      {
-        brand, category, name, price_pen, price_usd
+
+let {
+  brand, category, name, price_pen, price_usd
+}=event
+
+try {
+  const collection = await admin.firestore().collection("products").add(
+    {
+      brand, category, name, price_pen, price_usd
     }
-      
+    
     );
-    return respond('ok');
+    console.log(collection)
+    console.log(collection.id)
+    // const result = await s3.upload(params).promise()
+
+    return respond(collection);
   } catch (error) {
     return respond(error);
   }
+ 
+
 }
 function respond(data: any, statusCode = 200) {
   return {
